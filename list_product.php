6542868a9e9b253248fa950d0464da74bb190580
TKhoa27010707 <tieukhoa271@gmail.com>
2022-03-07 10:53:41 +0700

    Sửa

--- list_product.php
+++ list_product.php
@@ -1,13 +1,24 @@
-<?php 
-
-    require_once("/entities/product.class.php");
-?>
-<?php
-    include_once("header.php");
-    $prods = Product::list_product();
-    foreach ( $prods as $item )
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="site.css">
+    <link rel="stylesheet" href="index.html">
+</head>
+<body>
+    <?php 
+        require_once("/entities/product.class.php");
+    ?>
+    <?php
+        include_once("header.php");
+        $prods = Product::list_product();
+        foreach ( $prods as $item )
     {
         echo"<p> tên sản phẩm".$item["ProductName"]."</p>";
     }
-    include_once("footer.php");
-?>
+        include_once("footer.php");
+    ?>
+</body>
+</html>
