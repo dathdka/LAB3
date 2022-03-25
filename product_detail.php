e7c826e84e0488783e10bbb78b4f97e97c12114a
TKhoa27010707 <tieukhoa271@gmail.com>
2022-03-25 09:56:56 +0700

    CSS Detail

--- product_detail.php
+++ product_detail.php
@@ -17,7 +17,7 @@ $cates = Category::list_categories();
     <div style="background-color: darkgray; text-align: center; display: block; padding: 0.2px 0 0.1px 0;">
             <h1>Sản phẩm</h1>
     </div>
-    <div class="col-sm-4" style="display:inline-block; text-align:center; margin:20px; padding:20px ;border:1px solid black; float:initial; width:100%;">
+    <div class="col-sm-4" style="display:inline-block; text-align:center; margin:20px; padding:20px ;border:1px solid black; float:initial;width: -webkit-fill-available;">
             <div class="product-img" style="float: left;">
                 <img src="<?php echo $prod["Picture"]; ?>" style="width: 300px;" />
             </div>  
@@ -36,17 +36,25 @@ $cates = Category::list_categories();
         <h2>SẢN PHẨM LIÊN QUAN</h2>
     </div>
 
-<div class="col-md-8" style="display:inline-block; text-align:center; margin:20px; padding:20px; ;border:1px solid black;  ">
+<div class="col-md-8">
     <?php
     foreach ($prod_relate as $item) {
     ?>
-        <a href="/LAB3/product_detail.php?id=<?php echo $item["ProductID"]; ?>">
-            <img src=<?php echo "/LAB3/".$item["Picture"]; ?> class="img-responsive" style="width:200px">
-        </a>
-        <p class="text-danger"> <?php echo $item["ProductName"]; ?></p>
-        <p class="text-info"><?php echo $item["Desscription"] ?></p>
-        <p class="text-info"><?php echo $item["Price"] ?></p>
-        <button class="btn btn-primary" type="button">Mua hang</button>
+    <div class="col-md-4 col-sm-6 col-xs-12 pro-loop" style="text-align: center; padding: 22px;">
+        <div class="product-block">
+            <div class="product-img">
+                <a href="/LAB3/product_detail.php?id=<?php echo $item["ProductID"]; ?>">
+                <img src=<?php echo "/LAB3/".$item["Picture"]; ?> class="img-responsive" style="width:200px">
+                </a>
+            </div>
+            <div class="product-detail clearfix">
+                <p class="text-danger"> <?php echo $item["ProductName"]; ?></p>
+                <p class="text-info"><?php echo $item["Desscription"] ?></p>
+                <p class="text-info"><?php echo $item["Price"] ?></p>
+            </div>
+                <button class="btn btn-primary" type="button">Mua hang</button>
+        </div>
+    </div>
     <?php
     }
     ?>
